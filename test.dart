7b3f1fcaa1adeb93d5be4b6e63e4f4264518b131
Andrii Komarychev <lkomarichev@gmail.com>
2024-12-23 21:58:57 +0200

    init

--- test.dart
+++ test.dart
@@ -1,29 +1,3 @@
 void main() {
-  final bird = Bird(hp: 100);
-}
-
-abstract class Animal {
-  final int hp;
-
-  void makeSound() {
-    print('Animal makes sound');
-  }
-
-  Animal({required this.hp});
-}
-
-class Cat extends Animal {
-  Cat({required super.hp});
-}
-
-class Dog extends Animal {
-  Dog({required super.hp});
-}
-
-class Bird extends Animal {
-  Bird({required super.hp});
-}
-
-class Fish extends Animal {
-  Fish({required super.hp});
+  print('Hello, Nikita!');
 }
