908aba77f3e815dabb3b4e13394dfa88d2dda660
Andrii Komarychev <lkomarichev@gmail.com>
2025-01-05 22:30:09 +0200

    implemented fizz buzz 3

---
+++ fizz_buzz_versions/3.dart
@@ -0,0 +1,5 @@
+void fizzBuzz3() {
+  for (int i = 1; i <= 100; i++) {
+    print(i);
+  }
+}
--- test.dart
+++ test.dart
@@ -1,5 +1,7 @@
+import 'fizz_buzz_versions/fizz_buzz_3.dart';
 import 'fizz_buzz_versions/fizz_buzz_if_else.dart';
 
 void main() {
-  fizzBuzzIfElse();
+  // fizzBuzzIfElse();
+  fizzBuzz3();
 }
