060a2c93a43b07bc2e56cdbf96dca138b3542dd3
Nikita_Vel <velikihnikita301@gmail.com>
2025-01-05 21:58:35 +0200

    implemented if else FizzBuzz

--- test.dart
+++ test.dart
@@ -1,29 +1,5 @@
+import 'fizz_buzz_versions/fizz_buzz_if_else.dart';
 
-
-
-
-void main() 
-{
-  for(int i = 0; i <= 100;i++)
-  {
-     if(i % 3 == 0 && i % 5 == 0)
-    {
-       print('FizzBuzz');
-    }
-    else if(i % 3 == 0)
-    {
-      print('Fizz');
-    }
-    else if(i % 5 == 0)
-    {
-      print('Buzz');
-    }
-    else 
-    {
-      print(i);
-    }
-  }
-   
-} 
-
- 
+void main() {
+  fizzBuzzIfElse();
+}
