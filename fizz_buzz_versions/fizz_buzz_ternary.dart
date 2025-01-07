c14c4e718d4d307996a15915e8ef76839b1139ad
Nikita_Vel <velikihnikita301@gmail.com>
2025-01-07 17:45:31 +0200

    Релиз fizzbuzz с помощью тернарного оператора

---
+++ fizz_buzz_versions/fizz_buzz_ternary.dart
@@ -0,0 +1,11 @@
+void main() {
+  for (int i = 1; i <= 100; i++) {
+    print((i % 15 == 0)
+        ? 'FizzBuzz'
+        : (i % 5 == 0)
+            ? 'Buzz'
+            : (i % 3 == 0)
+                ? 'Fizz'
+                : i);
+  }
+}
