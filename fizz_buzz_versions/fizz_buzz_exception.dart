c6d3d8843034c1ee28d4434a2026a4efb577c410
Nikita_Vel <velikihnikita301@gmail.com>
2025-01-08 23:54:34 +0200

    renamed file

--- fizz_buzz_versions/fizz_buzz_exception.dart
+++ fizz_buzz_versions/fizz_buzz_exception.dart
@@ -1,23 +1,4 @@
 void main() {
-  for (int i = 1; i <= 100; i++) {
-    bool fizz = i % 3 == 0;
-
-    bool buzz = i % 5 == 0;
-
-    switch ((fizz, buzz)) {
-      case (true, true):
-        print('FizzBuzz');
-        break;
-      case (true, false):
-        print('Fizz');
-
-        break;
-      case (false, true):
-        print("Buzz");
-        break;
-      default:
-        print(i);
-        break;
-    }
-  }
+  
+  
 }
