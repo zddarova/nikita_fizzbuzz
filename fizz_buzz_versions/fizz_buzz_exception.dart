0b2486d64af912464a270998ab2a233739ca2216
Nikita_Vel <velikihnikita301@gmail.com>
2025-01-10 22:12:26 +0200

    FizzBuzz exception

--- fizz_buzz_versions/fizz_buzz_exception.dart
+++ fizz_buzz_versions/fizz_buzz_exception.dart
@@ -1,4 +1,28 @@
-void main() {
+void main(){
+  for(int i = 1;i <= 100;i++)
+  {
+    try
+    {
+      if(i % 15 == 0) throw 'FizzBuzz';
+
+      if(i % 5 == 0) throw 'Buzz';
+
+      if(i % 3 == 0) throw 'Fizz';
+
+      print(i);
+
+      }
+      catch(error)
+      {
+        print(error);
+      }
+
+
+    }
+    
+
+  }
+
+
   
-  
-}
+
